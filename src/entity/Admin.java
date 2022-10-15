c945e53abb9e53f74723468ddd25d6fe6b3b464a
Darryll Chong <34591796+ChaozFlamez@users.noreply.github.com>
2022-10-16 02:42:57 +0800

    Add files via upload

---
+++ src/entity/Admin.java
@@ -0,0 +1,30 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Admin implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private String userid;
+	private String password;
+	
+	public Admin(String userid, String password) {
+		this.userid = userid;
+		this.password = password;
+	}
+	
+	public String getUserID() {
+		return this.userid;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public boolean equals(Object o) {
+		if (o instanceof Admin) {
+			Admin a = (Admin)o;
+			return (getUserID().equals(a.getUserID()));
+		}
+		return false;
+	}
+}
