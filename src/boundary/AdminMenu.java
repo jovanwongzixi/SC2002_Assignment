020832630f8f222c53ddb3b65b5ebb5a73c1c85d
Darryll Chong <34591796+ChaozFlamez@users.noreply.github.com>
2022-10-16 15:27:59 +0800

    Update AdminMenu.java

--- src/boundary/AdminMenu.java
+++ src/boundary/AdminMenu.java
@@ -24,6 +24,9 @@ public class AdminMenu {
 				System.out.println("(2) ----------------      Edit movie listing");
 				System.out.println("(3) ----------------      Edit cinema showtimes");
 				System.out.println("(4) ----------------      Configure system settings");
+				if(!loggedIn) {
+					System.out.println("(5) ----------------      Return to main menu");
+				}
 				System.out.printf("\nOption: ");
 						
 				choice = sc.nextInt();
@@ -31,14 +34,14 @@ public class AdminMenu {
 				switch (choice) {
 					case 1:
 						if(!loggedIn) {
-							System.out.println("Enter user ID: ");
+							System.out.printf("\nEnter user ID: ");
 							String userid = sc.next();
-							System.out.println("Enter password: ");
+							System.out.printf("Enter password: ");
 							String password = sc.next();
 						
 							loggedIn = login(userid, password);
 						} else {
-							System.out.println("Successfully logged out! Returning to main menu...");
+							System.out.println("Successfully logged out! Returning to main menu...\n");
 							loggedIn = false;
 							toggle = false;
 							MainMenu.run();
@@ -74,12 +77,21 @@ public class AdminMenu {
 							systemConfig();
 						}
 						break;
+					case 5:
+						if(!loggedIn) {
+							System.out.println("Returning to main menu...\n");
+							toggle = false;
+							MainMenu.run();
+						} else {
+							System.out.println("Option does not exist. Please key in a valid option!\n");
+						}
+						break;
 					default:
-						System.out.println("Option does not exist. Please key in a valid option!");
+						System.out.println("Option does not exist. Please key in a valid option!\n");
 				}
 			} catch (InputMismatchException e) {
 				sc.nextLine();
-				System.out.println("Invalid input. Please make sure to enter an integer value!");
+				System.out.println("Invalid input. Please make sure to enter an integer value!\n");
 			}
 			
 		} while(toggle);	
