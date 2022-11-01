1f0e8e05a44c70e84ad096ac937107062eee3dfa
ucolinmee <80253369+ucolinmee@users.noreply.github.com>
2022-11-01 22:26:51 +0800

    Update CinemaLayout.java

--- CinemaLayout.java
+++ CinemaLayout.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class CinemaLayout2 {
+public class CinemaLayout {
     private int ROW = 8;
     private int COL = 8;
     public char[][] seatingPlan = new char[ROW][COL];
@@ -12,17 +12,38 @@ public class CinemaLayout2 {
         }
 
     public void displaySeats() {
-        System.out.println("     A     B     C     D     E     F     G     H  ");
-        System.out.println("   _____ _____ _____ _____ _____ _____ _____ _____");
-        for (int i=0; i<ROW; i++) {
-            System.out.println("  |     |     |     |     |     |     |     |     |");
+        System.out.println("     A     B     C     D           E     F     G     H  ");
+        System.out.println("   _______________________       _______________________");
+
+        // Print couple seats
+        for (int i=0; i<2; i++) {
+            System.out.println("  |           |           |     |           |           |");
+            System.out.print(i+1 + " |");
+            for (int j=0; j<COL; j++) {
+                if (j%2==1 && j<4)
+                    System.out.print("  " + seatingPlan[i][j] + "  |");
+                else if (j==4)
+                    System.out.print("     |  " + seatingPlan[i][j] + "  ");
+                else if (j%2==0 && j>=4)
+                    System.out.print("|  " + seatingPlan[i][j] + "  ");
+                else
+                    System.out.print("   " + seatingPlan[i][j] + "  ");
+            }
+            System.out.print("|");
+            System.out.println(" ");
+            System.out.println("  |___________|___________|     |___________|___________|");
+        }
+
+        // Print single seats
+        for (int i=2; i<ROW; i++) {
+            System.out.println("  |     |     |     |     |     |     |     |     |     |");
             System.out.print(i+1 + " ");
             for (int j=0; j<COL; j++) {
                 System.out.print("|  " + seatingPlan[i][j] + "  ");
             }
-            System.out.print("|");
+            System.out.print("|     |");
             System.out.println(" ");
-            System.out.println("  |_____|_____|_____|_____|_____|_____|_____|_____|");
+            System.out.println("  |_____|_____|_____|_____|     |_____|_____|_____|_____|");
         }
     }
 
@@ -35,26 +56,49 @@ public class CinemaLayout2 {
         {
             case 1: 
             System.out.print("Enter column (A-H): ");
-            char addCol = sc.next().charAt(0);
+            char add = sc.next().charAt(0);
+            int addCol = add - 'A';
             System.out.print("Enter row (1-8): ");
             int addRow = sc.nextInt();
             
-            if (seatingPlan[addRow - 1][addCol - 'A'] == ' ') { // removeCol - 'A' gets the integer equivalent to the alphabetical col
-                seatingPlan[addRow - 1][addCol - 'A'] = 'X';
-                System.out.println("Successfully booked seat.");
-            } 
+            if (seatingPlan[addRow-1][addCol]==' ') {
+                if (addRow==1 || addRow==2) { // Double seat vacancy
+                    seatingPlan[addRow - 1][addCol] = 'X';
+                    if (addCol%2 == 0) 
+                        seatingPlan[addRow - 1][addCol + 1] = 'X';
+                    else 
+                        seatingPlan[addRow - 1][addCol - 1] = 'X';
+                    System.out.println("Successfully booked seat.");
+                }
+                else { // Single seat vacancy
+                    seatingPlan[addRow - 1][addCol] = 'X';
+                    System.out.println("Successfully booked seat.");
+                }
+            }
             else { System.out.println("Seat has already been taken, please choose another seat."); }
             break;
 
+
             case 2:
             System.out.print("Enter column (A-H): ");
-            char removeCol = sc.next().charAt(0);
+            char remove = sc.next().charAt(0);
+            int removeCol = remove - 'A';
             System.out.print("Enter row (1-8): ");
             int removeRow = sc.nextInt();
-          
-            if (seatingPlan[removeRow - 1][removeCol - 'A'] == 'X') { // removeCol - 'A' gets the integer equivalent to the alphabetical col
-                seatingPlan[removeRow - 1][removeCol - 'A'] = ' ';
-                System.out.println("Booking has been successfully cancelled.");
+
+            if (seatingPlan[removeRow-1][removeCol]=='X') {
+                if (removeRow==1 || removeRow==2) { // Double seat vacancy
+                    seatingPlan[removeRow - 1][removeCol] = ' ';
+                    if (removeCol%2 == 0) 
+                        seatingPlan[removeRow - 1][removeCol + 1] = ' ';
+                    else 
+                        seatingPlan[removeRow - 1][removeCol - 1] = ' ';
+                    System.out.println("Booking has been successfully cancelled.");
+                }
+                else { // Single seat vacancy
+                    seatingPlan[removeRow - 1][removeCol] = 'X';
+                    System.out.println("Booking has been successfully cancelled.");
+                }
             }
             else { System.out.println("Seat selected has not been booked, please choose a valid seat."); } 
             break;
