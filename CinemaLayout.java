275cfe946f1ee5a7bb47615af4723093cb3d6199
ucolinmee <80253369+ucolinmee@users.noreply.github.com>
2022-11-01 14:48:26 +0800

    Update CinemaLayout - grid version

--- CinemaLayout.java
+++ CinemaLayout.java
@@ -1,34 +1,29 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class CinemaLayout {
-    private int ROW = 12;
-    private int COL = 15;
-    char[][] seatingPlan = new char[ROW][COL];
+public class CinemaLayout2 {
+    private int ROW = 8;
+    private int COL = 8;
+    public char[][] seatingPlan = new char[ROW][COL];
 
     public void initialiseSeats() {
-    for (char[] row : seatingPlan)
-        Arrays.fill(row, 'X');
-    }
-
+        for (char[] row : seatingPlan)
+            Arrays.fill(row, ' ');
+        }
 
     public void displaySeats() {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("        " + "A B C D    E F G H I J    K L M N O");
-        System.out.println("==============================================");
-        for (int i=0; i<ROW; i++){
-            int row = i + 1;
-            if (row < 10) { System.out.print("  "+ row + "  |  "); }
-            else { System.out.print(" "+ row + "  |  "); }
-                
-            for (int j=0; j<COL; j++){
-                if (j == 3 || j == 9) { System.out.print(seatingPlan[i][j] + "    "); }  
-                else { System.out.print(seatingPlan[i][j] + " "); }
+        System.out.println("     A     B     C     D     E     F     G     H  ");
+        System.out.println("   _____ _____ _____ _____ _____ _____ _____ _____");
+        for (int i=0; i<ROW; i++) {
+            System.out.println("  |     |     |     |     |     |     |     |     |");
+            System.out.print(i+1 + " ");
+            for (int j=0; j<COL; j++) {
+                System.out.print("|  " + seatingPlan[i][j] + "  ");
             }
+            System.out.print("|");
             System.out.println(" ");
+            System.out.println("  |_____|_____|_____|_____|_____|_____|_____|_____|");
         }
-        System.out.println("==============================================");  
     }
 
     public void getSelection() {
@@ -39,26 +34,26 @@ public class CinemaLayout {
         switch (userChoice) 
         {
             case 1: 
-            System.out.print("Enter column (A-O): ");
+            System.out.print("Enter column (A-H): ");
             char addCol = sc.next().charAt(0);
-            System.out.print("Enter row (1-12): ");
+            System.out.print("Enter row (1-8): ");
             int addRow = sc.nextInt();
             
-            if (seatingPlan[addRow - 1][addCol - 'A'] == 'X') { // removeCol - 'A' gets the integer equivalent to the alphabetical col
-                seatingPlan[addRow - 1][addCol - 'A'] = '-';
+            if (seatingPlan[addRow - 1][addCol - 'A'] == ' ') { // removeCol - 'A' gets the integer equivalent to the alphabetical col
+                seatingPlan[addRow - 1][addCol - 'A'] = 'X';
                 System.out.println("Successfully booked seat.");
             } 
             else { System.out.println("Seat has already been taken, please choose another seat."); }
             break;
 
             case 2:
-            System.out.print("Enter column (A-O): ");
+            System.out.print("Enter column (A-H): ");
             char removeCol = sc.next().charAt(0);
-            System.out.print("Enter row (1-12): ");
+            System.out.print("Enter row (1-8): ");
             int removeRow = sc.nextInt();
           
-            if (seatingPlan[removeRow - 1][removeCol - 'A'] == '-') { // removeCol - 'A' gets the integer equivalent to the alphabetical col
-                seatingPlan[removeRow - 1][removeCol - 'A'] = 'X';
+            if (seatingPlan[removeRow - 1][removeCol - 'A'] == 'X') { // removeCol - 'A' gets the integer equivalent to the alphabetical col
+                seatingPlan[removeRow - 1][removeCol - 'A'] = ' ';
                 System.out.println("Booking has been successfully cancelled.");
             }
             else { System.out.println("Seat selected has not been booked, please choose a valid seat."); } 
